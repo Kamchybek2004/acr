62345a0565dd5131d643aa31d3b963ea477b0ff0
Kamchy <kamchy2501@gmail.com>
2026-02-25 00:30:56 +0600

    Добавление import gettext_lazy

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from .managers import UserManager
 
