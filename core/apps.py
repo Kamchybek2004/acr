09b9fd3a936d2059094ae11b22f8e371941c9c14
Kamchy <kamchy2501@gmail.com>
2026-02-25 02:10:07 +0600

    -6

--- core/apps.py
+++ core/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    verbose_name = "Учебное управление"
 
     def ready(self):
         import core.signals
