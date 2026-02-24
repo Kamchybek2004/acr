3ab97beae215c7679cef0a5c640ea5f541ec3fa7
Kamchy <kamchy2501@gmail.com>
2026-02-25 00:02:53 +0600

    -2

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,11 +3,12 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from unfold.admin import ModelAdmin
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(BaseUserAdmin, ModelAdmin):
     ordering = ("email",)
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
